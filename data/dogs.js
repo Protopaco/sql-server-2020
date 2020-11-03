c111eaf049116d34af1e05e75b55a46c5d90c469
Paul Stevens <twinkisan@hotmail.com>
2020-11-02 19:17:47 -0800

    mvp

---
+++ data/dogs.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    name: 'Marcus',
+    age: 6,
+    weight: 25,
+    good_boy: true,
+  },
+  {
+    name: 'Paco',
+    age: 6,
+    weight: 11,
+    good_boy: true,
+  },
+  {
+    name: 'Benjamin',
+    age: 10,
+    weight: 10,
+    good_boy: true,
+  }
+];
--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,6 +1,6 @@
 const client = require('../lib/client');
 // import our seed data:
-const animals = require('./animals.js');
+const dogs = require('./dogs.js');
 const usersData = require('./users.js');
 const { getEmoji } = require('../lib/emoji.js');
 
@@ -25,12 +25,12 @@ async function run() {
     const user = users[0].rows[0];
 
     await Promise.all(
-      animals.map(animal => {
+      dogs.map(dog => {
         return client.query(`
-                    INSERT INTO animals (name, age, owner_id)
-                    VALUES ($1, $2, $3);
+                    INSERT INTO dogs (name, age, weight, good_boy, owner_id)
+                    VALUES ($1, $2, $3, $4, $5);
                 `,
-          [animal.name, animal.age, user.id]);
+          [dog.name, dog.age, dog.weight, dog.good_boy, user.id]);
       })
     );
 
