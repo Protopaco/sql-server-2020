f834479a48fc87e48ea0571541a1809ea3547db8
Paul Stevens <twinkisan@hotmail.com>
2020-11-02 18:46:23 -0800

    back to square one

---
+++ data/animals.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    name: 'Marcus',
+    cool_factor: 3,
+  },
+  {
+    name: 'Paco',
+    cool_factor: 4,
+  },
+  {
+    name: 'Benjamin',
+    cool_factor: 10,
+  }
+];
--- data/create-tables.js
+++ data/create-tables.js
@@ -17,12 +17,10 @@ async function run() {
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE dogs (
+                CREATE TABLE animals (
                     id SERIAL PRIMARY KEY NOT NULL,
                     name VARCHAR(512) NOT NULL,
-                    age INTEGER NOT NULL,
-                    weight INTEGER NOT NULL,
-                    good_boy BOOL NOT NULL,
+                    cool_factor INTEGER NOT NULL,
                     owner_id INTEGER NOT NULL REFERENCES users(id)
             );
         `);
