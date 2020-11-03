a9f921ea760c824de2c3e91c0c78f1e774226d0c
Paul Stevens <twinkisan@hotmail.com>
2020-11-02 17:52:18 -0800

    added dogs

---
+++ data/dogs.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    name: 'Marcus',
+    age: 6,
+    weight: 25,
+    good_boy: true,
+  },
+  {
+    name: 'Paco',
+    age: 6,
+    weight: 11,
+    good_boy: true,
+  },
+  {
+    name: 'Benjamin',
+    age: 9,
+    weight: 10,
+    good_boy: true,
+  }
+];
