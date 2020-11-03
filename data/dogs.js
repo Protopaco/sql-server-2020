bfb963e0617071968b31de424abd62eb8462c88b
Paul Stevens <twinkisan@hotmail.com>
2020-11-03 08:52:01 -0800

    fixes image URLs

--- data/dogs.js
+++ data/dogs.js
@@ -4,14 +4,14 @@ module.exports = [
     age: 6,
     weight: 25,
     good_boy: true,
-    img_src: 'https://i.ibb.co/Mn8Kdn1/IMG-9161.jpg'
+    img_src: 'https://i.pinimg.com/originals/09/bc/ba/09bcbac03c8723b106b1ae14de3027ed.jpg'
   },
   {
     name: 'Paco',
     age: 6,
     weight: 11,
     good_boy: true,
-    img_src: 'https://i.ibb.co/Mn8Kdn1/IMG-9161.jpg'
+    img_src: 'https://wagspetadoption.org/wp-content/uploads/2018/12/48314211_2418978178173894_3787177238194028544_n.jpg'
 
   },
   {
@@ -19,7 +19,7 @@ module.exports = [
     age: 10,
     weight: 10,
     good_boy: true,
-    img_src: 'https://i.ibb.co/Mn8Kdn1/IMG-9161.jpg'
+    img_src: 'https://www.pdsa.org.uk/media/9895/gallery-2-dachshund-standing-outside.jpg'
 
   }
 ];
