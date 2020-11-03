a92fde8a9ae731215634288bcfeabd4c8468b160
Paul Stevens <twinkisan@hotmail.com>
2020-11-02 23:02:44 -0800

    changed images

--- data/dogs.js
+++ data/dogs.js
@@ -4,14 +4,14 @@ module.exports = [
     age: 6,
     weight: 25,
     good_boy: true,
-    img_src: 'https://www.dropbox.com/s/r5xw48o3ct8b4fw/IMG_9161.jpeg?dl=0'
+    img_src: 'https://ibb.co/0qB15q9'
   },
   {
     name: 'Paco',
     age: 6,
     weight: 11,
     good_boy: true,
-    img_src: 'https://www.dropbox.com/s/r5xw48o3ct8b4fw/IMG_9161.jpeg?dl=0'
+    img_src: 'https://ibb.co/0qB15q9'
 
   },
   {
@@ -19,7 +19,7 @@ module.exports = [
     age: 10,
     weight: 10,
     good_boy: true,
-    img_src: 'https://www.dropbox.com/s/r5xw48o3ct8b4fw/IMG_9161.jpeg?dl=0'
+    img_src: 'https://ibb.co/0qB15q9'
 
   }
 ];
