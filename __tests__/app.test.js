ea314555e778c7fb4849769df6069017a557fc22
Paul Stevens <twinkisan@hotmail.com>
2020-11-03 13:45:02 -0800

    add id functionality & tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,56 +9,92 @@ const client = require('../lib/client');
 describe('app routes', () => {
   describe('routes', () => {
     let token;
-  
+
     beforeAll(async done => {
       execSync('npm run setup-db');
-  
+
       client.connect();
-  
+
       const signInData = await fakeRequest(app)
         .post('/auth/signup')
         .send({
           email: 'jon@user.com',
           password: '1234'
         });
-      
+
       token = signInData.body.token;
-  
+
       return done();
     });
-  
+
     afterAll(done => {
       return client.end(done);
     });
 
-  test('returns animals', async() => {
+    test('returns entire dogs array', async () => {
 
-    const expectation = [
+      const expectation = [
+        {
+          id: 1,
+          owner_id: 1,
+          name: 'Marcus',
+          age: 6,
+          weight: 25,
+          good_boy: true,
+          img_src: 'https://i.pinimg.com/originals/09/bc/ba/09bcbac03c8723b106b1ae14de3027ed.jpg'
+        },
+        {
+          id: 2,
+          owner_id: 1,
+          name: 'Paco',
+          age: 6,
+          weight: 11,
+          good_boy: true,
+          img_src: 'https://wagspetadoption.org/wp-content/uploads/2018/12/48314211_2418978178173894_3787177238194028544_n.jpg'
+
+        },
+        {
+          id: 3,
+          owner_id: 1,
+          name: 'Benjamin',
+          age: 10,
+          weight: 10,
+          good_boy: true,
+          img_src: 'https://www.pdsa.org.uk/media/9895/gallery-2-dachshund-standing-outside.jpg'
+
+        }
+      ];
+
+      const data = await fakeRequest(app)
+        .get('/dogs/')
+        .expect('Content-Type', /json/)
+        .expect(200);
+      expect(data.body).toEqual(expectation);
+    });
+
+    test('returns single dog object', async () => {
+
+      const expectation =
       {
-        'id': 1,
-        'name': 'bessie',
-        'coolfactor': 3,
-        'owner_id': 1
-      },
-      {
-        'id': 2,
-        'name': 'jumpy',
-        'coolfactor': 4,
-        'owner_id': 1
-      },
-      {
-        'id': 3,
-        'name': 'spot',
-        'coolfactor': 10,
-        'owner_id': 1
+        id: 2,
+        owner_id: 1,
+        name: 'Paco',
+        age: 6,
+        weight: 11,
+        good_boy: true,
+        img_src: 'https://wagspetadoption.org/wp-content/uploads/2018/12/48314211_2418978178173894_3787177238194028544_n.jpg'
+
       }
-    ];
+        ;
 
-    const data = await fakeRequest(app)
-      .get('/animals')
-      .expect('Content-Type', /json/)
-      .expect(200);
+      const data = await fakeRequest(app)
+        .get('/dogs/2')
+        .expect('Content-Type', /json/)
+        .expect(200);
+      expect(data.body).toEqual(expectation);
+    });
 
-    expect(data.body).toEqual(expectation);
   });
+
+
 });
